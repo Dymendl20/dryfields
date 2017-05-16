d65c9bbad3ff8fbd59fb3ec249e177095e8023d7
EveX1 <evekill@free.fr>
2017-05-16 14:37:07 +0200

    emit buy water

--- views/GameView.js
+++ views/GameView.js
@@ -1,8 +1,18 @@
-function View(game) {
+function GameView(game) {
+    EventEmitter.call(this);
     this.game = game;
+    this.init();
 }
 
-View.prototype.waterBought = function(data) {
-    liters = $();
-    this.emit('water-bought', { quantity: liters })
+View.prototype = Object.create(EventEmitter.prototype);
+View.prototype.conscructor = View;
+
+View.prototype.init = function() {
+    $('#menu-achat').submit(this.waterBought.bind(this))
+}
+
+View.prototype.waterBought = function(e) {
+    e.preventDefault();
+    var liters = $('#eau-qty').val();
+    this.emit('water-bought', { quantity: liters });
 }
