b2906c458248565dbcad47fb0ad0b02b019ed318
EveX1 <evekill@free.fr>
2017-05-16 22:44:19 +0200

    GameController.js

---
+++ controllers/GameController.js
@@ -0,0 +1,12 @@
+function GameController(field, user) {
+    this.field = field;
+    this.user = user;
+}
+
+UserController.prototype.update = function (label, data) {
+
+    if (label == 'recolte_mure') {
+
+        this.model.game.setRecolter(data.recolter);
+    }
+}
